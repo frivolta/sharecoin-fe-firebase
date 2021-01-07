9e68abe36b377e4b3dc4408507251820d6692aa5
frivolta <rivoltafilippo@gmail.com>
2021-01-07 11:11:39 +0100

    Adding loading fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthNavigator } from "./navigators/authNavigator";
+import * as Font from "expo-font";
+import { Loading } from "./screens/Loading";
+
 export default function App() {
+  const [isLoadingFonts, setIsLoadingFonts] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    loadFonts();
+  }, []);
+
+  async function loadFonts() {
+    await Font.loadAsync({
+      PoppinsRegular: require("./assets/fonts/Poppins-Regular.ttf"),
+      PoppinsMedium: require("./assets/fonts/Poppins-Medium.ttf"),
+      PoppinsBold: require("./assets/fonts/Poppins-Bold.ttf"),
+    });
+    setIsLoadingFonts(false);
+  }
+
+  if (isLoadingFonts) {
+    return <Loading />;
+  }
+
   return (
     <NavigationContainer>
       <AuthNavigator />
--- screens/Loading.tsx
+++ screens/Loading.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export const CreateAccount = () => {
+export const Loading = () => {
   return (
     <View>
-      <Text style={{ fontFamily: "PoppinsBold" }}>Create account</Text>
+      <Text>Loading</Text>
     </View>
   );
 };
